3b437b050db695dc490d1c6e9ec0a1a0e6ff3507
Michael Cinus <michaelcinus@gmail.com>
2020-07-24 13:06:23 +0200

    correzione bug button

--- js/script.js
+++ js/script.js
@@ -61,13 +61,15 @@ function stampaFeste(dataInizio) {
 function init() {
 
     var i = 0;
+    $('.fa-arrow-circle-left').hide();
 
     var data = ["2018-01-01", "2018-02-01", "2018-03-01", "2018-04-01", "2018-05-01", "2018-06-01", "2018-07-01", "2018-08-01", "2018-09-01", "2018-10-01", "2018-11-01", "2018-12-01"];
     var mesi = ['Gennaio', 'Febbraio', 'Marzo', ' Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']
 
     $('.fa-arrow-circle-right').click(function(){
+        $('.fa-arrow-circle-left').show();
+
          i++; 
-         console.log(i);
          if (i == 11){
             $('.fa-arrow-circle-right').hide();
         }
@@ -79,6 +81,7 @@ function init() {
 
         });
     $('.fa-arrow-circle-left').click(function(){
+        $('.fa-arrow-circle-right').show();
         i--; 
         if (i == 0){
             $('.fa-arrow-circle-left').hide();
